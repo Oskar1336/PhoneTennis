0a764e2dc950e3ee98c4460904dad0c77bdda28e
LinusHakansson <hakansson-24@live.se>
2018-03-06 12:31:06 +0100

    Testing sensors

---
+++ app/src/main/java/ptcorp/ptapplication/game/Sensors/SensorListener.java
@@ -0,0 +1,34 @@
+package ptcorp.ptapplication.game.Sensors;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+import ptcorp.ptapplication.game.GameActivity;
+
+/**
+ * Created by LinusHakansson on 2018-03-06.
+ */
+
+public class SensorListener implements SensorEventListener {
+    private SensorResult mListener;
+
+    public SensorListener(SensorResult sensorResult) {
+        this.mListener = sensorResult;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        mListener.onUpdate(event);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public interface SensorResult{
+        void onUpdate(SensorEvent event);
+    }
+}
