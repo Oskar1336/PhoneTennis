359b069994d3cb3f8e512d9e0e453aa66fce7b9a
ag7829 <ponta45@hotmail.com>
2018-03-09 11:47:51 +0100

    Går att skicka

--- app/src/main/java/ptcorp/ptapplication/main/pojos/GameScore.java
+++ app/src/main/java/ptcorp/ptapplication/main/pojos/GameScore.java
@@ -1,10 +1,15 @@
 package ptcorp.ptapplication.main.pojos;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.io.Serializable;
+
 /**
  * Created by Pontus on 2018-02-22.
  */
 
-public class GameScore {
+public class GameScore implements Serializable, Parcelable{
 
     private String player1, player2, date;
     private int score1, score2;
@@ -16,6 +21,27 @@ public class GameScore {
         this.score1 = score1;
         this.score2 = score2;
     }
+
+
+    protected GameScore(Parcel in) {
+        player1 = in.readString();
+        player2 = in.readString();
+        date = in.readString();
+        score1 = in.readInt();
+        score2 = in.readInt();
+    }
+
+    public static final Creator<GameScore> CREATOR = new Creator<GameScore>() {
+        @Override
+        public GameScore createFromParcel(Parcel in) {
+            return new GameScore(in);
+        }
+
+        @Override
+        public GameScore[] newArray(int size) {
+            return new GameScore[size];
+        }
+    };
 
     public String getPlayer1() {
         return player1;
@@ -35,5 +61,19 @@ public class GameScore {
 
     public int getScore2() {
         return score2;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(player1);
+        parcel.writeString(player2);
+        parcel.writeString(date);
+        parcel.writeInt(score1);
+        parcel.writeInt(score2);
     }
 }
