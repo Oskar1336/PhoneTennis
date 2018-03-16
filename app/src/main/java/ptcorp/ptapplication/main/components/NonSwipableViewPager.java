6a1a586e5cd14dce415f9e777a5343283ea44079
LinusHakansson <linus.haknsn@gmail.com>
2018-03-16 14:06:31 +0100

    Disable viewPager swiping

---
+++ app/src/main/java/ptcorp/ptapplication/main/components/NonSwipableViewPager.java
@@ -0,0 +1,26 @@
+package ptcorp.ptapplication.main.components;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by LinusHakansson on 2018-03-16.
+ */
+
+public class NonSwipableViewPager extends ViewPager {
+    public NonSwipableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return false;
+    }
+}
